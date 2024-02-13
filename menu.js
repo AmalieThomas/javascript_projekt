2f3aa54039c6a998aeba848e899d0d874eb209ef
Ceciliekknudsen <ceciliekfknudsen@gmail.com>
2024-02-13 11:13:53 +0100

    Update menu.js

---
+++ menu.js
@@ -0,0 +1,40 @@
+function opensidebar() {
+    document.getElementById('sidebar').style.left = '0px';
+    document.getElementById('overlay').style.display = 'block';
+    console.log = "menu is open"
+  }
+
+  function luksidebar() {
+    document.getElementById('sidebar').style.left = '-350px';
+    document.getElementById('overlay').style.display = 'none';
+  }
+
+
+
+  function drop() {
+    let damedrop = document.getElementById("dame");
+    if (damedrop.style.display == "block") {
+        damedrop.style.display = "none";
+    } else {
+        damedrop.style.display = "block";
+    }
+}
+
+
+function drop2() {
+    let herredrop = document.getElementById("herre");
+    if (herredrop.style.display == "block") {
+        herredrop.style.display = "none";
+    } else {
+        herredrop.style.display = "block";
+    }
+}
+
+function drop3() {
+    let kidsdrop = document.getElementById("kids");
+    if (kidsdrop.style.display == "block") {
+        kidsdrop.style.display = "none";
+    } else {
+        kidsdrop.style.display = "block";
+    }
+}
