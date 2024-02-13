d508c8f43a527f8b997c51f2d9f063416fe95e78
AmalieThomas <Amalie.thomas@hotmail.dk>
2024-02-13 10:13:42 +0100

    uyguy

--- heart.js
+++ heart.js
@@ -1,13 +1,9 @@
-document.getElementById("heart1").addEventListener("mouseover", fullHeart);
 
-function fullHeart() {
-    heart1.classList.remove("fa-regular");
-    heart1.classList.add("fa-solid");
-  }
-
-  document.getElementById("heart1").addEventListener("mouseout", emptyHeart);
-
- function emptyHeart() {
-    heart1.classList.remove("fa-solid");
-    heart1.classList.add("fa-regular");
+function toggleHeart() {
+   var heartIcon = document.getElementById('heartIcon');
+   heartIcon.classList.toggle('fa-regular');
+   heartIcon.classList.toggle('fa-solid');
  }
+
+
+
