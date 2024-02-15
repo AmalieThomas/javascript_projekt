ad5e1a1f3b3afb39ebf936a53350bbdab7a4ea4e
AmalieThomas <Amalie.thomas@hotmail.dk>
2024-02-15 10:00:42 +0100

    edfef

--- shopping-bag.js
+++ shopping-bag.js
@@ -1,21 +1,88 @@
-/*let size = ["XXS", "XS", "S", "M", "L/XL", "2XL", "2XL", "3XL", "4XL", "5XL"]
-
-function addItem () {
-
-}
-*/
-
-var x = 4
+let selectedSize = null
 
 function openBag() {
-  if ( x>2 ) { 
-    var popup = document.getElementById("myPopup2");
-    popup.classList.toggle("show");
-  }
-  else {
-    var popup = document.getElementById("myPopup1");
-    popup.classList.toggle("show");
+    if ( added == true ) { 
+      let popup = document.getElementById("myPopup2");
+      popup.classList.toggle("show");
+    }
+    else {
+      let popup = document.getElementById("myPopup1");
+      popup.classList.toggle("show");
+    }
 
+    document.getElementById("shopping-bag").style.color = ""
+  }
+  
+  let sizes = ["XXS", "XS", "S", "M", "L/XL", "2XL", "3XL", "4XL", "5XL"]
+  
+  function sizeToggle(element, sizes) {
+  
+      if(element.style.backgroundColor == "grey"){
+          resetBackgroundColors()
+          selectedSize = null;
+      }
+       
+      else {
+          resetBackgroundColors()
+          element.style.backgroundColor = "grey";
+          selectedSize = sizes;
+      }
   }
 
+  function resetBackgroundColors() {
+    let sizeButtons = document.getElementsByClassName("size-button");
+    for (let i = 0; i < sizeButtons.length; i++) {
+        sizeButtons[i].style.backgroundColor = "";
+    }
 }
+  
+    let antalProduct = 0
+  
+    let pris = 551.00
+  
+    let leveringValue = 19.00
+
+    let added = null
+  
+  
+  function addToBag() {
+  
+      if (selectedSize != null){
+
+          antalProduct++
+
+          addProduct(selectedSize)
+
+          udregn()
+
+      }
+  
+      else {
+          alert("vælg størrelse")
+      }
+  
+    }
+  
+  function addProduct(size){
+  
+      let productDiv = document.createElement('div');
+      productDiv.innerHTML = '<h3>Compression Shine Stirrup Pant</h3>' +'<p>Str: ' + size + '</p>' +'<p>Pris: ' + pris + ' kr</p>'+ '<p>';
+  
+      document.getElementById('pant').appendChild(productDiv);
+
+      document.getElementById("shopping-bag").style.color = "red"
+
+      added = true
+  }
+  
+  
+  function udregn(){
+      let ordreValue = pris * antalProduct
+  
+      let ialtValue = parseInt(leveringValue + ordreValue)
+    
+      document.getElementById("ordre-value").innerHTML = ordreValue + " Kr";
+      document.getElementById("levering-value").innerHTML = leveringValue + " Kr";
+      document.getElementById("ialt-value").innerHTML = ialtValue + " Kr";
+  
+  }
